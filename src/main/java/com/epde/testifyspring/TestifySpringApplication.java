41abc4056fc9a1dfeca560a19919a8fa0a0685ab
Md. Mosfikur Rahman <mdmosfikurrahman.cse@gmail.com>
2023-04-28 22:50:42 +0600

    TestifySpring: Initial Commit  - Created Sample Project  - Written Test classes for all except for Controller

---
+++ src/main/java/com/epde/testifyspring/TestifySpringApplication.java
@@ -0,0 +1,13 @@
+package com.epde.testifyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestifySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestifySpringApplication.class, args);
+    }
+
+}
